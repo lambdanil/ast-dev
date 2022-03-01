2693692b1f1ad546b5a595cb4389adb3ae7704e7
CuBeRJAN <59669705+CuBeRJAN@users.noreply.github.com>
2022-03-01 14:06:54 +0100

    Add files via upload

--- astpk.py
+++ astpk.py
@@ -183,6 +183,7 @@ def sync_tree(treename):
     children = [str(node.name) for node in anytree.LevelOrderIter(fstree)]
     print(children)
     print("fstree", fstree)
+    print_tree(fstree)
     for child in children: # This runs for the tree itself, fix later (doesn't cause issues)
         par = (anytree.find(fstree, filter_=lambda node: (str(node.name) + "x") in (str(child) + "x")))
         spar = str(par).split("/")
