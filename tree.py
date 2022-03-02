0fabcb3d6278e5031f421ab3339999ecb3fb46ea
CuBeRJAN <59669705+CuBeRJAN@users.noreply.github.com>
2022-03-02 14:27:25 +0100

    Add files via upload

--- tree.py
+++ tree.py
@@ -16,37 +16,25 @@ def import_tree_file(treename):
     tree = ast.literal_eval(treefile.readline())
     return(tree)
 
-# Print out tree with descriptions
 def print_tree(tree):
     for pre, fill, node in anytree.RenderTree(tree):
-        if os.path.isfile(f"/root/images/{node.name}-desc"):
-            descfile = open(f"/root/images/{node.name}-desc","r")
-            desc = descfile.readline()
-            descfile.close()
-        else:
-            desc = ""
-        print("%s%s - %s" % (pre, node.name, desc))
+        print("%s%s" % (pre, node.name))
 
+# Print out tree with descriptions
 # Add to root tree
 def append_base_tree(tree,val):
     add = anytree.Node(val, parent=tree.root)
 
 # Add child to node
 def add_node_to_parent(tree, id, val):
-    par = (anytree.find(tree, filter_=lambda node: (str(node.name)+"x") in (str(id)+"x")))
+    par = (anytree.find(tree, filter_=lambda node: (str(node.name)+"x") in (str(id)+"x"))) # Not entirely sure how the lambda stuff here works, but it does ¯\_(ツ)_/¯
     add = anytree.Node(val, parent=par)
-    
-def add_node_to_level(tree,id, val): # Broken, likely useless, probably remove later
-    par = (anytree.find(tree, filter_=lambda node: (str(node.name) + "x") in (str(id) + "x")))
-    spar = str(par).split("/")
-    nspar = (spar[len(spar)-2])
-    npar = (anytree.find(tree, filter_=lambda node: (str(node.name) + "x") in (str(nspar) + "x")))
-    add = anytree.Node(val, parent=npar)
 
 # Remove node from tree
 def remove_node(tree, id):
     par = (anytree.find(tree, filter_=lambda node: (str(node.name)+"x") in (str(id)+"x")))
     par.parent = None
+    print(par)
 
 # Save tree to file
 def write_tree(tree):
@@ -56,19 +44,40 @@ def write_tree(tree):
     fsfile.write(str(to_write))
 
 # Return list of children for node
-def return_children(tree, id):
+def return_all_children(tree, id):
     children = []
-    par = (anytree.find(tree, filter_=lambda node: (str(node.name)+"x") in (str(id)+"x")))
+    par = (anytree.find(tree, filter_=lambda node: (str(id)+"x") in (str(node.name)+"x")))
+    #print(par.children)
     for child in anytree.PreOrderIter(par):
         children.append(child.name)
     return (children)
 
-
-def return_only_children(tree, id):
+def return_children(tree, id):
     children = []
-    par = (anytree.find(tree, filter_=lambda node: "0" in node.name, maxlevel=2))
-    for child in anytree.PreOrderIter(par):
-        children.append(child.name)
+    importer = DictImporter() # Dict importer
+    par = (anytree.find(tree, filter_=lambda node: (str(id)+"x") in (str(node.name)+"x")))
+    #print(par, par.children)
+    children = []
+    try:
+        cstr = str(par.children)
+    except:
+        return(children)
+    cstr = cstr.replace("(","")
+    cstr = cstr[:-1]
+    scstr = cstr.split("Node'/root/")
+    nscstr = []
+    for item in scstr:
+        item = item.replace(",", "")
+        item = item.replace("')", "")
+        sitem = item.split(f"{id}/")
+        if item != "" and len(sitem) > 1:
+            children.append(sitem[1])
+        else:
+            item = sitem[0]
+            item = item.replace("AnyNodename='","")
+            item = item.replace(" ", "")
+            if item != "":
+                children.append(item)
     return (children)
 
 # Main function
@@ -78,20 +87,14 @@ def main(args):
     global fstree # Currently these are global variables, fix sometime
     global fstreepath # ---
     fstreepath = str("tree.txt") # Path to fstree file
-    print("fs",importer.import_(import_tree_file(fstreepath)))
     fstree = (importer.import_(import_tree_file(fstreepath))) # Import fstree file
-    append_base_tree(fstree, "test")
-    #add_node_to_parent(fstree, "test", "test2")
-    #add_node_to_parent(fstree, "test", "test3")
-    #add_node_to_parent(fstree, "test2", "test6")
-    add_node_to_parent(fstree, "4", "test5")
-    #append_base_tree(fstree, "test10")
-    add_node_to_parent(fstree, "0", "1")
-    print(exporter.export(fstree))
-    print("fffs",str(fstree))
+    add_node_to_parent(fstree,"0", "1")
+    add_node_to_parent(fstree, "4", "2")
+    add_node_to_parent(fstree, "4", "7")
+    add_node_to_parent(fstree, "2", "5")
+    add_node_to_parent(fstree, "2", "6")
     print_tree(fstree)
-    print(return_children(fstree,"0"))
-    print(return_only_children(fstree,"0"))
-    # Recognize argument and call appropriate function
+    #print(return_all_children(fstree, "4"))
+    print(return_children(fstree, "2"))
 
 main(args)
